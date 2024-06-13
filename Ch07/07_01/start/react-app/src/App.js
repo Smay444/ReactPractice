0f135001024b295bac0faf67da47dd91c333e62d
smay444 <stacipugh@gmail.com>
2024-06-13 16:48:16 -0400

    Updated Link

--- Ch07/07_01/start/react-app/src/App.js
+++ Ch07/07_01/start/react-app/src/App.js
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div>
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Contact">Contact</Link>
       </nav>
